f4fea0a57460396f7ff60c922a425e6280520d1c
kazbek-d <kazbek_d@hotmail.com>
2025-05-17 06:50:19 +0200

    Set state

--- .deps/MyContract.sol
+++ .deps/MyContract.sol
@@ -2,5 +2,13 @@
 pragma solidity 0.8.30;
 
 contract MyContract {
+    uint number;
 
+    function setNumber(uint _number) public {
+        number = _number;
+    }
+
+    function getNumber() view public returns (uint) {
+        return number;
+    }
 }
