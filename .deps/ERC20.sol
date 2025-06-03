906e22e7e71941eb1fea0dc5e01c0e385074bde6
kazbek-d <kazbek_d@hotmail.com>
2025-06-03 07:08:29 +0200

    Added events

--- .deps/ERC20.sol
+++ .deps/ERC20.sol
@@ -4,6 +4,11 @@ pragma solidity 0.8.30;
 import "hardhat/console.sol";
 
 contract Erc20Token {
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address spender, uint256 value);
+
+    //address public constant feeAddress = address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
     string public constant name = "Erc20Token";
     string public constant symbol = "ERC20";
     uint8 public constant decimals = 18;
@@ -13,6 +18,8 @@ contract Erc20Token {
 
     function _transfer(address from, address to, uint256 amount) private returns(bool) {
         require(balancesOf[from] >= amount, "ERC20: Insufficient sender balance");
+
+        emit Transfer(from, to, amount);
 
         balancesOf[from] -= amount;
         balancesOf[to] += amount;
@@ -27,11 +34,13 @@ contract Erc20Token {
     function transferFrom(address sender, address recipient, uint256 amount) external returns(bool) {
         require(allowance[sender][msg.sender] >= amount, "ERC20: Insufficient allowance");
         allowance[sender][msg.sender] -= amount;
+        emit Approval(sender, msg.sender, allowance[sender][msg.sender]);
         return _transfer(sender, recipient, amount);
     }
 
     function approve (address spender, uint256 amount) external returns (bool){
         allowance[msg.sender][spender] += amount;
+        emit Approval(msg.sender, spender, amount);
         return true;
     } 
 
